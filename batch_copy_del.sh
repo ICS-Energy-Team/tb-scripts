fcc1c8cc6a37257007ceb82c2e49c8745bf2f6a1
bazenkov <n.bazenkov@yandex.ru>
2020-04-16 14:54:46 +0300

    Month is a variable now

--- batch_copy_del.sh
+++ batch_copy_del.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-for d in {7..28}
+month=3
+last_day=31
+echo "Copy ts_kv for month=$month"
+d=1
+#for d in {1..5};
+while [ "$d" -lt "$last_day" ];
 do
   d_next=$(($d+1))
-  filename="/media/nfs/arxiv/ts_kv_2020-02-$d.csv.gz"
-  copy_del.sh "2020-02-$d 00:00:00" "2020-02-$d_next 00:00:00" $filename >~/log/batch.log 2>~/log/batch.log 
+  filename="/media/nfs/arxiv/ts_kv_2020-$month-$d.csv.gz"
+  echo "Copying to $filename"
+  copy_del.sh "2020-$month-$d 00:00:00" "2020-$month-$d_next 00:00:00" $filename >~/log/batch.log 2>~/log/batch.log 
+  d=$d_next
 done
-#Copy 29-th Feb
-filename="/media/nfs/arxiv/ts_kv_2020-02-29.csv.gz"
-copy_del.sh "2020-02-29 00:00:00" "2020-03-1 00:00:00" $filename
+#Copy last day
+filename="/media/nfs/arxiv/ts_kv_2020-$month-$last_day.csv.gz"
+next_month=$(($month+1))
+echo "Copying to $filename"
+copy_del.sh "2020-$month-$last_day 00:00:00" "2020-$next_month-1 00:00:00" $filename
