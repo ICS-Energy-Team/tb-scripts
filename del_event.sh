09c6babef9f7299759906ab4ab020c6d49c214bf
bazenkov <n.bazenkov@yandex.ru>
2020-04-30 22:07:26 +0300

    Bugs with dates fixed

--- del_event.sh
+++ del_event.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #Usage
 #./del_event.sh '2020-04-01 00:00' '2020-04-02 15:10:30'
-ts_1=$(($(date -d "$1" +%s)*1000))
-ts_2=$(($(date -d "$2" +%s)*1000))
 echo "Deleting events after $1 and before $2"
-psql -U thingsboard -d thingsboard -h localhost -c "DELETE FROM event WHERE uuid_tstz(event_uid)>=$ts_1 AND uuid_tstz(event_uid)<$ts_2);" 
-#psql -U thingsboard -h localhost -d thingsboard -W --output /media/nfs/devices.out -c "\copy device TO /media/nfs/devices.csv WITH DELIMITER ';' CSV HEADER"
+psql -U thingsboard -d thingsboard -h localhost -c "DELETE FROM event WHERE uuid_tstz(event_uid)>='$1' AND uuid_tstz(event_uid)<'$2';" 
+echo "Event deleted"
