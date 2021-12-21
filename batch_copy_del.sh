adbdc992f2e8c997c88882321d54e787f9a9cd51
Nikolay Bazenkov <n.bazenkov@yandex.ru>
2021-12-21 14:11:32 +0300

    batch_copy_del is changed, now it is just copy without deletion

--- batch_copy_del.sh
+++ batch_copy_del.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #The script doesn't handle December!
-month="04"
-last_day=30
+month="10"
+last_day=31
 year=2021
 base_folder="/mnt/nfs/arxiv"
 folder=$base_folder/$year.$month
+logfile="/home/collective/log/batch_copy-$year-$month.log"
+exec > $logfile
+exec 2>&1
 echo "Copy ts_kv for month=$month, year=$year to $folder"
 if [ ! -d $folder ]; then
         echo "Create new folder: $folder"
@@ -12,21 +15,22 @@ if [ ! -d $folder ]; then
 fi
 d=1
 #for d in {1..5};
-logfile="/home/collective/log/batch-$year-$month.log"
 touch $logfile
 while [ "$d" -lt "$last_day" ];
 do
   d_next=$(($d+1))
   filename="$folder/ts_kv_$year-$month-$d.csv.gz"
-  echo "Copying to $filename, log is saved to $logfile"
-  copy_del.sh "$year-$month-$d 00:00:00" "$year-$month-$d_next 00:00:00" $filename 
+  echo "Copying to $filename"
+  copy_ts.sh "$year-$month-$d 00:00:00" "$year-$month-$d_next 00:00:00" $filename
+  #copy_del.sh "$year-$month-$d 00:00:00" "$year-$month-$d_next 00:00:00" $filename 
   d=$d_next
 done
 #Copy last day
 filename="$folder/ts_kv_$year-$month-$last_day.csv.gz"
 next_month=$(($month+1))
-echo "Copying to $filename, log is saved to $logfile"
-copy_del.sh "$year-$month-$last_day 00:00:00" "$year-$next_month-1 00:00:00" $filename
+echo "Copying to $filename"
+copy_ts.sh "$year-$month-$last_day 00:00:00" "$year-$next_month-1 00:00:00" $filename
+#copy_del.sh "$year-$month-$last_day 00:00:00" "$year-$next_month-01 00:00:00" $filename
 #Copy devices
 now_date=$(date -d "now" +"%Y-%m-%d")
 file_device="$folder/devices.csv"
